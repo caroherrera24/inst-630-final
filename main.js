4b0e8aabc424adc5af249ff8123bdcb16e6c0695
Carolina Herrera <ch.caro.her@gmail.com>
2024-11-08 14:19:26 -0500

    add stars

--- main.js
+++ main.js
@@ -12,17 +12,25 @@ const globe = new ThreeGlobe()
 let CLOUDS_ALT = 0.004;
 let cloudRadius = globe.getGlobeRadius() * (1 + CLOUDS_ALT);
 const cloudGeometry = new THREE.SphereGeometry(cloudRadius, 75, 75);
-const clouds =  new THREE.Mesh(cloudGeometry);
-const cloudLoader = new THREE.TextureLoader();
-
-cloudLoader.load("public/images/earth-clouds.png", cloudsTexture => {
-  clouds.material = new THREE.MeshPhongMaterial({
-    map: cloudsTexture,
+const cloudLoader = new THREE.TextureLoader().load("public/images/earth-clouds.png");
+const cloudMaterial = new THREE.MeshPhongMaterial({
+    map: cloudLoader,
     transparent: true,
     opacity: 0.8
   });
-});
+const clouds =  new THREE.Mesh(cloudGeometry, cloudMaterial);
 globe.add(clouds);
+
+
+// add stars
+const starLoader = new THREE.TextureLoader().load("public/images/starfield.png");
+const starGeometry = new THREE.SphereGeometry(1000, 50, 50);
+const starMaterial = new THREE.MeshPhongMaterial({
+  map: starLoader,
+  side: THREE.DoubleSide,
+  shininess: 0
+});
+const starField = new THREE.Mesh(starGeometry, starMaterial);
 
 // setup lights
 const ambientLight = new THREE.AmbientLight(0xe3e3e3, 5);
@@ -42,6 +50,7 @@ const scene = new THREE.Scene();
 scene.add(globe);
 scene.add(ambientLight);
 scene.add(directionalLight);
+scene.add(starField);
 
 // setup camera
 const camera = new THREE.PerspectiveCamera();
@@ -52,6 +61,7 @@ camera.position.set(250,250,10);
 // add camera controls
 const tbControls = new TrackballControls(camera, renderer.domElement);
 tbControls.minDistance = 100;
+tbControls.maxDistance = 1000;
 tbControls.rotateSpeed = 5;
 tbControls.zoomSpeed = 0.8;
 
@@ -59,7 +69,7 @@ tbControls.zoomSpeed = 0.8;
 (function animate() {
   // rotate the globe and clouds
   globe.rotation.y += .0005;
-  clouds.rotation.y -= 0.00025 * Math.PI / 180;
+  clouds.rotation.y += 0.004 * Math.PI / 180;
 
   tbControls.update();
   renderer.render(scene, camera);
